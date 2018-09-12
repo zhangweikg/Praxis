7e7ab58a6a2b0e8ffcdc8fd27e03fbe3603d5aa7
xlf112233 <309373556@qq.com>
2018-09-12 18:00:32 +0800

    1234

--- 星艺装饰2/xydec/Projs.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('input, textarea').placeholder();
-    $('#openChant').click(function () {
-        doyoo.util.openChat(' g=10067026');
-        return false;
-    });
-});
